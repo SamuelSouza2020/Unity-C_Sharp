865f841731a40e5045ca1b62be3b2a37bf2f7b22
Samuel Souza <souzasamuel623@gmail.com>
2021-03-22 09:17:30 -0300

    Código GetAxis

--- Move 2D/Move3.cs
+++ Move 2D/Move3.cs
@@ -1,2 +1,18 @@
 //Invés de usar o GetKey, iremos utilizar o GetAxis (método pronto do Unity)
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move1 : MonoBehaviour
+{
+    //A velocidade pode ser ajustada no Inspector da Unity.
+    public float v = 3;
+
+    void Update()
+    {
+        //GetAxis Horizontal voce poderá modificar pelo Edit>Project Settings>Input Manager>Horizontal
+        float H = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(H * Time.deltaTime * v, 0, 0));
+    }
+}
