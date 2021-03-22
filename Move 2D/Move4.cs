6f567731dcbe6697fd62531458d0a87db8bb33a5
Samuel Souza <souzasamuel623@gmail.com>
2021-03-22 09:45:38 -0300

    Update Move4.cs

--- Move 2D/Move4.cs
+++ Move 2D/Move4.cs
@@ -11,12 +11,13 @@ public class move1 : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            //Rigidbody é o corpo físico do GameObjeto (Player)
+            //Rigidbody é o corpo rígido do GameObjeto (Player)
             GetComponent<Rigidbody2D>().velocity = new Vector2(v, 0);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            //Velocity está sendo aplicada no corpo rígido
             GetComponent<Rigidbody2D>().velocity = new Vector2(-v, 0);
         }
     }
