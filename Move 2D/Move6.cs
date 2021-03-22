fae38a862ad5dbb21d56fc5788c1020fb9d8fc08
Samuel Souza <souzasamuel623@gmail.com>
2021-03-22 11:00:23 -0300

    Joystick Axis

--- Move 2D/Move6.cs
+++ Move 2D/Move6.cs
@@ -1,6 +1,8 @@
-/*Com joystick é um pouco semelhante ao Mouse. Mas nele voce terá que seguir alguns passos.
+/*
+Com joystick é um pouco semelhante ao Mouse. Mas nele voce terá que seguir alguns passos.
 Dentro da engine Unity, entre em Edit>Project Settings>Input Manager
-Em Input Manager você irá até um Submit, após apertar nele o primeiro campo dele é Name, você mudará o nome para Direcional (Pode ser outro).*/
+Em Input Manager você irá até um Submit, após apertar nele o primeiro campo dele é Name, você mudará o nome para Direcional (Pode ser outro).
+*/
 
 using System.Collections;
 using System.Collections.Generic;
@@ -12,9 +14,12 @@ public class move1 : MonoBehaviour
 
     void Update()
     {
-        /*Coloque o mesmo nome que você alterou no Submit dentro do GetAxis
+        /*
+        Coloque o mesmo nome que você alterou no Submit dentro do GetAxis
         Dentro do Input Manager>Direcional você limpará os campo Alt, positive e negative button
         Colocará o Type como Joystick Axis e pesquisará o mapa do controle que vai utilizar como joystick
+        Na opção Axi que fica abaixo do Type você coloca seleciona o nome do botão mostrado no mapa
+        Nesse exemplo irei demonstrar o Horizontal do controle "X axis" ou "6th axis"
         */
         float H = Input.GetAxis("Direcional");
 
