05f7ac753ecea767ee48577028a62d0bd4c71545
Samuel Souza <souzasamuel623@gmail.com>
2021-03-22 11:45:10 -0300

    Pulo Básico = Flappy Bird

--- Jumping 2D/Pulo.cs
+++ Jumping 2D/Pulo.cs
@@ -1,21 +1,32 @@
+//Antes de executar o código seu GameObject (Sprite) deve está com Rigidbody2D e collisor2D
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move1 : MonoBehaviour
+public class Pulo : MonoBehaviour
 {
 
+    //Aqui está chamando um corpo rígido
     Rigidbody2D rig;
 
     void Start()
     {
+        //Aqui pega o corpo rígido do sprite que está com esse script. 
         rig = GetComponent<Rigidbody2D>();
     }
 
+    /*
+    Mesmo aplicando força, será usado o Update pois nesse exemplo de código será chamado
+    constantimente (Estilo Flappy Bird), para não ter atraso ou falha será usado o Update que é 
+    chamado a cada frame. Já o FixedUpdate é chamado em um intervalo de tempo fixo, por isso é mais usado
+    quando o código é lento (mais calculos físicos).
+    */
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            //Voce pode aplicar força no Vetor X e Y (new Vector(x,y))
             rig.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
         }
     }
