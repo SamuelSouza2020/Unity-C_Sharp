adaf9a001a430db4e62779ed2a88ee753ed971b3
Samuel Souza <souzasamuel623@gmail.com>
2021-03-22 10:33:01 -0300

    Move with Mouse

--- Move 2D/Move5.cs
+++ Move 2D/Move5.cs
@@ -1,3 +1,5 @@
+//Um exemplo básico de como utilizar o Mouse para movimentar o Player.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +11,7 @@ public class move5 : MonoBehaviour
 
     void Update()
     {
+        //Aqui você está chamando o método pronto da Unity, que está localizado em: Edit>Project Settings>Input Manager
         float H = Input.GetAxis("Mouse X");
         float V = Input.GetAxis("Mouse Y");
 
